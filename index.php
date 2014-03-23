75541cea39c17d4d5c3e8fc787d18fa5cdb4648a
Michael Cheng <michael.cheng@mig33global.com>
2014-03-23 21:43:23 +0800

    Pass first test.

--- index.php
+++ index.php
@@ -8,3 +8,8 @@ $r3 = new Router;
 $r3->get('/', function() {
   return 'Hello, World';
 });
+
+$r3->get('/blog', function() {
+  $payload = array( 'message' => 'Listing all posts', 'data'=> array() );
+  return json_encode($payload);
+});
