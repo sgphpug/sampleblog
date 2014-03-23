9054476ed72a41f4c8eeeeafec145482c318fa9d
Michael Cheng <michael.cheng@mig33global.com>
2014-03-23 22:44:58 +0800

    Refactor to use functional programming.

--- lib/SampleBlog/BlogController.php
+++ lib/SampleBlog/BlogController.php
@@ -15,43 +15,43 @@ class BlogController implements Routable
 
   public function get($id=null)
   {
-    try
-    {
+    $response = $this->res;
+    return $this->runAction(function() use ($response){
+      $response->data = array();
       $posts = Post::all();
       foreach($posts as $post)
       {
-        $this->res->data[] = $post->to_array();
+        $response->data[] = $post->to_array();
       }
-    }
-    catch(\Exception $ex)
-    {
-      $this->res->message = "Exception: " . $ex->getMessage();
-    }
-    return $this->res;
+      $response->message = "Listing of posts";
+    });
   }
 
   public function delete($id=null)
   {
-    try
-    {
-      $post = Post::find($id);
-      $post->delete();
-      $this->res->message = 'Deleted';
-    }
-    catch(\Exception $ex)
-    {
-      $this->res->message = "Exception: " . $ex->getMessage();
-    }
-    return $this->res;
+    $response = $this->res;
+    return $this->runAction(function() use ($id, $response){
+        $post = Post::find($id);
+        $post->delete();
+        $response->message = 'Deleted';
+      });
   }
 
   public function post($id=null)
   {
-    try
-    {
+    $response = $this->res;
+    return $this->runAction(function() use ($response){
       $attributes = $_POST;
       Post::create($attributes);
-      $this->res->message = "Created new post";
+      $response->message = "Created new post";
+    });
+  }
+
+  private function runAction($func)
+  {
+    try
+    {
+      $func();
     }
     catch(\Exception $ex)
     {
